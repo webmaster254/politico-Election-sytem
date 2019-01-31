1a38402b8bc518837eca198f54f86e75902bbc79
joehacker <josephm2800@gmail.com>
2019-01-31 21:36:00 +0300

    [feature #163625109] added admin dashboard

--- UI/JS/scripts.js
+++ UI/JS/scripts.js
@@ -8,21 +8,3 @@
                 x.className = "topnav";
             }
             }
-
-                function openlogin() {
-                document.getElementById("login").style.display = "block";
-                }
-
-                function opensignup() {
-                document.getElementById("signup").style.display = "block";
-                }
-
-            // When the user clicks anywhere outside of the modal, close it
-            window.onclick = function(event) {
-            if (event.target == modal) {
-            modal.style.display = "none";
-            }
-            }
-            function check(form)/*function to check userid & password*/
-{
- 
